8816befa2c2d604a3941e95d61a9ef444af485b7
Pitt Wu <pitt.wu@hlwtech.com.tw>
2022-04-19 11:18:35 +0800

    feat: install plugin & init style

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import './App.css';
+import { AppWrapper } from 'styles/AppStyle';
 
 function App() {
-  return <div className='App'>Auto Complete</div>;
+  return <AppWrapper className='App'>Hello App</AppWrapper>;
 }
 
 export default App;
---
+++ src/styles/AppStyle.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+const AppWrapper = styled.main`
+  display: flex;
+  justify-content: center;
+  margin-top: 8rem;
+  width: 100%;
+  height: 100%;
+`;
+
+export { AppWrapper };
